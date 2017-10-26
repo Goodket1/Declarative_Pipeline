e24b484036535f560561320273d53d38591c3687
Yaroslav Koretskyi <y.koretskyi@Yaroslavs-MBP.kyiv.epam.com>
2017-10-26 13:09:38 +0300

    edited Jenkinsfile_deploy

--- script.groovy
+++ script.groovy
@@ -1,7 +1,8 @@
 
-def metadata = new XmlSlurper().parse("http://10.17.11.223::8091/artifactory/snapshots/com/efsavage/web_app/maven-metadata.xml")
+def metadata = new XmlSlurper().parse('http://10.17.11.223::8091/artifactory/snapshots/com/efsavage/web_app/maven-metadata.xml')
 def list = []
 metadata.versioning.versions.version.each{
   list.add(it)
 }
 return list.reverse(true)
+println list
