d55b7e8731e3dec4fee76809bd340cbd7cd82495
Yaroslav Koretskyi <y.koretskyi@Yaroslavs-MBP.kyiv.epam.com>
2017-10-26 13:26:49 +0300

    edited Jenkinsfile_deploy

--- script.groovy
+++ script.groovy
@@ -1,8 +1,8 @@
-def URL = 'http://10.17.11.223::8091/artifactory/snapshots/com/efsavage/web_app/maven-metadata.xml'
+def URL = 'http://10.17.11.223:8091/artifactory/snapshots/com/efsavage/web_app/maven-metadata.xml'
 def metadata = new XmlSlurper().parse(URL)
 def list = []
 metadata.versioning.versions.version.each{
   list.add(it)
 }
 return list.reverse(true)
-println list
+println list[0]
