7b76a7a54fb08e6b0b1663b94baf59092bd5bc83
Yaroslav Koretskyi <y.koretskyi@gmail.com>
2017-10-28 22:31:47 +0300

    edited deploy

--- script.groovy
+++ script.groovy
@@ -6,3 +6,26 @@ metadata.versioning.versions.version.each{
 }
 return list.reverse(true)
 println list[0]
+
+
+pipeline{
+  agent  {
+    node {
+     label 'CI_env'
+        customWorkspace '/var/jenkins'
+    }
+ }
+stages ('build') {
+  stage ('push') {
+    steps {
+      script{
+
+def server = Artifactory.newServer url: 'https://artifactory.legsup.co.uk/artifactory/', username: 'admin', password: 'Tf1cda11'
+def artDocker= Artifactory.docker username: 'admin', password: 'Tf1cda11'
+def dockerInfo = artDocker.push 'artifactory.legsup.co.uk:8082/hello-world:latest', 'docker'
+    server.publishBuildInfo(dockerInfo)
+}
+}
+}
+}
+}
