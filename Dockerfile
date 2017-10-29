668a82d40272a091062c7d0d19eec76bf4b1d5e4
Yaroslav Koretskyi <y.koretskyi@gmail.com>
2017-10-29 16:26:28 +0200

    edited deploy

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ USER root
 # Place the code version inside the webapps directory
 ARG PACKAGE_VERSION
 RUN echo "${PACKAGE_VERSION}" >> /usr/local/tomcat/webapps/version.txt
-COPY project.war /tmp/project.war
-RUN rm -r /usr/local/tomcat/webapps/ROOT/* && cp  /tmp/project.war /usr/local/tomcat/webapps/ROOT/
+#COPY project.war /tmp/project.war
+RUN rm -r /usr/local/tomcat/webapps/ROOT/*
+#&& cp  /tmp/project.war /usr/local/tomcat/webapps/ROOT/
+COPY project.war /usr/local/tomcat/webapps/ROOT/
 
 CMD ["catalina.sh", "run"]
--- script.groovy
+++ script.groovy
@@ -1,31 +1,9 @@
-def URL = 'http://10.17.11.223:8091/artifactory/snapshots/com/efsavage/web_app/maven-metadata.xml'
+def URL = 'https://artifactory.legsup.co.uk/artifactory/api/docker/docker/v2/web_app/tags/list'
+
 def metadata = new XmlSlurper().parse(URL)
 def list = []
-metadata.versioning.versions.version.each{
+metadata.tags.each{
   list.add(it)
 }
 return list.reverse(true)
-println list[0]
 
-
-pipeline{
-  agent  {
-    node {
-     label 'CI_env'
-        customWorkspace '/var/jenkins'
-    }
- }
-stages ('build') {
-  stage ('push') {
-    steps {
-      script{
-
-def server = Artifactory.newServer url: 'https://artifactory.legsup.co.uk/artifactory/', username: 'admin', password: 'Tf1cda11'
-def artDocker= Artifactory.docker username: 'admin', password: 'Tf1cda11'
-def dockerInfo = artDocker.push 'artifactory.legsup.co.uk:8082/hello-world:latest', 'docker'
-    server.publishBuildInfo(dockerInfo)
-}
-}
-}
-}
-}
