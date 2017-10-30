fed9841df0d82ea44c7dfb3aa12015311dfc1e61
Yaroslav Koretskyi <y.koretskyi@gmail.com>
2017-10-30 17:17:42 +0200

    edited deploy

--- tag.sh
+++ tag.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-var = docker tag artifactory.legsup.co.uk:8082/web_app:$1 artifactory.legsup.co.uk:8082/web_app:2
-eval $var
+docker tag artifactory.legsup.co.uk:8082/web_app:$1 artifactory.legsup.co.uk:8082/web_app:$2
