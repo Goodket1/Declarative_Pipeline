0c5bb220c2374d72cb08638072bad49d4ddafa40
Yaroslav Koretskyi <y.koretskyi@gmail.com>
2017-11-02 23:26:07 +0200

    adde post step

--- script1.groovy
+++
@@ -1,7 +0,0 @@
-def URL = 'https://artifactory.legsup.co.uk/artifactory/version/com/efsavage/web_app/maven-metadata.xml'
-def metadata = new XmlSlurper().parse(URL)
-def list = []
-metadata.versioning.versions.version.each{
-  list.add(it)
-}
-return list.reverse(true)
